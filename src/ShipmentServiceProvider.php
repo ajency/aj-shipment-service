fad469b44a4ad8cd59c5a0ed4264baad64f65e88
tanvi salkar <tanvi.s@ajency.in>
2019-02-21 12:26:29 +0530

    setup shipment package

---
+++ src/ShipmentServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ajency\AjShipmentService;;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShipmentServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
